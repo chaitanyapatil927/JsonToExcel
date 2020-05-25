db4fdfc591cbf86687653f87e0b8e9107a7bde38
Chaitanya Patil <chaitanya.patil@sakec.ac.in>
2020-05-25 13:50:08 +0530

    for diamond

--- server.js
+++ server.js
@@ -2,7 +2,28 @@ var json2xls = require('json2xls');
 const fs = require("fs")//npm install fs
 var rawFile = fs.readFileSync("./datas.json")//dir of your json file as param
 var raw = JSON.parse(rawFile)
+console.log(rawFile)
 console.log(raw)
-var xls = json2xls(raw);
+var xls = json2xls(raw,{
+    fields : {
+        styleNumber:'Number',
+        images:{
+            main:'string',
+            white:'string',
+            yellow:'string',
+            rose:'string'
+        },
+        diamondWeight:'Number',
+        diamondCount:'Number',
+        goldWeight:'Number',
+        designDetails:{
+            new:Boolean,
+            featured:Boolean,
+            highestSelling:Boolean,
+            fancyDiamond:Boolean
+        },
+        company:'string',
+        favouriteCount:'Number'}
+ });
 
 fs.writeFileSync('data.xlsx', xls, 'binary');
